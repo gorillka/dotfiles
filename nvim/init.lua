d355385ff9f0686bf193c68c51fb86f287a18e54
Artem Orynko <artem.orynko@customertimes.com>
2025-03-15 11:39:36 +0200

    neovim basic setup

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+require 'core.options' -- Load core options
+require 'core.configs' -- Load general options
+require 'user'         -- Load user customizations
+require 'core.keymaps' -- Load general keymaps
+require 'core.lazy'    -- Load general plugins
+require 'core.colors'  -- Load general colors
---
+++ nvim/lua/core/lazy.lua
@@ -0,0 +1,70 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require("lazy").setup({
+    -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+
+    -- NOTE: Plugins can also be added by using a table,
+    -- with the first argument being the link and the following
+    -- keys can be used to configure plugin behavior/loading/etc.
+    --
+    -- Use `opts = {}` to force a plugin to be loaded.
+
+    -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+    --
+    -- This is often very useful to both group configuration, as well as handle
+    -- lazy loading plugins that don't need to be loaded immediately at startup.
+    --
+    -- For example, in the following configuration, we use:
+    --  event = 'VimEnter'
+    --
+    -- which loads which-key before all the UI elements are loaded. Events can be
+    -- normal autocommands events (`:help autocmd-events`).
+
+    -- NOTE: Plugins can specify dependencies.
+    --
+    -- The dependencies are proper plugin specifications as well - anything
+    -- you do for a plugin at the top level, you can do for a dependency.
+    --
+    -- Use the `dependencies` key to specify the dependencies of a particular plugin
+    spec = {
+        -- add LazyVim and import its plugins
+        {
+            -- import/override with your plugins
+            { import = "plugins" },
+        },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    -- install = { colorscheme = { "onedark" } },
+    -- automatically check for plugin updates
+    checker = {
+        enabled = true, -- automatically check for plugin updates
+    },
+})
