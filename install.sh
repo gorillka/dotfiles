7fd2741a115f1f239bcfeccb1d9c9bb167af0086
gorilka <a.orynko@gmail.com>
2025-09-01 09:39:08 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,37 @@
 #### SkyLab Helper Installation Script by ####
 #### Gorilka (https://github.com/gorillka) ####
 
+safe_source() {
+    local url="$1"
+    local content
+
+    content=$(curl -fsSL "$url")
+
+    if [ $? -eq 0 ]; then
+        eval "$content"
+    else
+        echo "ERROR: $url" >&2
+        return 1
+    fi
+}
+
 # Clear screen and pause briefly for user experience
-# clear && sleep 1
+clear && sleep 1
 
-source <(curl -fsSL https://raw.githubusercontent.com/gorillka/dotfiles/refs/heads/${PROFILE}/misc/core.func)
+# Load core functionality functions from remote repository
+safe_source "https://raw.githubusercontent.com/gorillka/dotfiles/${PROFILE:-main}/misc/core.func"
+safe_source "https://raw.githubusercontent.com/gorillka/dotfiles/${PROFILE:-main}/misc/msg.func"
+
+# Load build-related functions from remote repository
+safe_source "https://raw.githubusercontent.com/gorillka/dotfiles/${PROFILE:-main}/misc/build.func"
+
+# Load additional utility functions from remote repository
+safe_source "https://raw.githubusercontent.com/gorillka/dotfiles/${PROFILE:-main}/misc/tools.func"
+safe_source "https://raw.githubusercontent.com/gorillka/dotfiles/${PROFILE:-main}/misc/install.func"
+
+# Initialize core functions
 load_core_func
-
-source <(curl -fsSL https://raw.githubusercontent.com/gorillka/dotfiles/refs/heads/${PROFILE}/misc/msg.func)
-
-source <(curl -fsSL https://raw.githubusercontent.com/gorillka/dotfiles/refs/heads/${PROFILE}/misc/build.func)
+# Initialize build functions
 load_build_func
-
-source <(curl -fsSL https://raw.githubusercontent.com/gorillka/dotfiles/refs/heads/${PROFILE}/misc/install.func)
+# # Execute the main installation process
 pre_install
