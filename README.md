ea861193e5d3c3bf64466537490b831d4effaca8
gorilka <a.orynko@gmail.com>
2025-09-02 13:20:52 +0300

    ..

---
+++ README.md
@@ -0,0 +1,5 @@
+# Install
+
+```bash
+curl -fsSL https://github.com/gorillka/dotfiles/raw/main/install.sh | bash
+```
---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#################################################
+#### SkyLab Helper Installation Script by   ####
+#### Gorilka (https://github.com/gorillka)  ####
+#################################################
+
+# Get the directory where this script is located and execute the startup function
+# This allows the script to work regardless of where it's called from
+source $(dirname "${BASH_SOURCE[0]}")/misc/startup.func
+startup
