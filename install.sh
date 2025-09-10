12c7563d33a4bb83c51897b4a61c7d9588bfed33
gorilka <a.orynko@gmail.com>
2025-09-10 22:56:51 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -19,13 +19,10 @@ if [ ! "$(command -v chezmoi)" ]; then
     else
         install $chezmoi /usr/local/bin
     fi
+    rm $chezmoi
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
-set -- init --apply --source="${script_dir}"
-
-echo "Running 'chezmoi $*'" >&2
-# exec: replace current process with chezmoi
-exec "$chezmoi" "$@"
+# exec: replace current process with chezmoi init
+exec "chezmoi" init --apply "--source=$script_dir"
