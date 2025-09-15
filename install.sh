4d8b19967e5f7bfcdaf570adb920b3003a093a50
gorilka <a.orynko@gmail.com>
2025-09-15 18:23:10 +0300

    ..

--- install.sh
+++ install.sh
@@ -21,8 +21,3 @@ if [ ! "$(command -v chezmoi)" ]; then
     fi
     rm $chezmoi
 fi
-
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "chezmoi" init --source=${script_dir} gorillka
